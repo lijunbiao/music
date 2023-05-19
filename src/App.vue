ad81de138b580de2fd6c04337c607796b6b4dbbc
xiaoli <xiaoli@qq.com>
2023-05-19 17:42:51 +0800

    完成头部

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
-       app
+        <!-- 头部 -->
+       <MHeader></Mheader>
   </div>
 </template>
-
+<script>
+import MHeader from './components/m-header/m-header.vue'
+export default {
+  components: {
+    MHeader
+  }
+}
+</script>
 <style lang="scss">
 @import "./common/scss/index.scss";
 </style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="m-header">
+         <!-- 标题 -->
+         <h1 class="text">VMUSICJIN</h1>
+         <!-- 左侧菜单图标 -->
+         <router-link to="/" class="mine">
+                <i class="iconfont icon-caidan"></i>
+         </router-link>
+          <!-- 右侧搜索图标 -->
+         <router-link to="/" class="search">
+                <i class="iconfont icon-search"></i>
+         </router-link>
+    </div>
+</template>
+<script>
+export default {
+  name: 'm-header'
+}
+</script>
+<style lang="scss" scoped>
+@import "../../common/scss/variable.scss";
+.m-header {
+   position: relative;
+   height: 44px;
+   width: 100%;
+   background: $color-theme; // 主题颜色：红色
+   text-align: center;
+   .text {
+     line-height: 44px;
+     font-weight: bold;
+     color: $color-text-l; // 白色
+     letter-spacing: 3px;
+   }
+   .mine {
+       position: absolute;
+       left: 0;
+       top: 0;
+       .iconfont {
+            color: $color-theme-l; // 白色
+            display: block;
+            padding: 11px;
+            font-size: 22px;
+       }
+   }
+   .search {
+       position: absolute;
+       right: 0;
+       top: 0;
+         .iconfont {
+            color: $color-theme-l; // 白色
+            display: block;
+            padding: 11px;
+            font-size: 22px;
+       }
+   }
+}
+</style>
